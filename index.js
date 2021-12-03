ae2bc29cb14b1bcd0a6899b849501ec59b7edfee
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-12-03 13:16:24 -0600

    commit tree d52ec59bc9dd3f7545013ff5f4982cd83e4460c3

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    const regEx = /[^A-Za-z\d-' ]/g;
+    return string.replaceAll(regEx, "");
+  }
+
+  static titleize(string) {
+    // define deny-list
+    const denyList = [
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+      "the",
+    ];
+
+    return string
+      .split(" ")
+      .map((word, index) => {
+        //  if word is not on deny-list or first word of sentence
+        if (!denyList.includes(word) || index === 0) {
+          //caplitalize then return
+          return Formatter.capitalize(word);
+        }
+        // else return string
+        return word;
+      })
+      .join(" ");
+  }
 }
